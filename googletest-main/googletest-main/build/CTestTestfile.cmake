65419f10c1c014939cd3202a0a8d1ec1bfb67db2
rk <kt.r713@gmail.com>
2023-11-14 01:49:03 +0900

    lib

---
+++ googletest-main/googletest-main/build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: Z:/lab/googletest-main/googletest-main
+# Build directory: Z:/lab/googletest-main/googletest-main/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("googlemock")
---
+++ googletest-main/googletest-main/build/googletest/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: Z:/lab/googletest-main/googletest-main/googletest
+# Build directory: Z:/lab/googletest-main/googletest-main/build/googletest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
